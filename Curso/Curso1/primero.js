d870408ba1aef3d744e53164b6a9bd27456b1204
Albres20 <david_albres20@hotmail.com>
2021-08-16 21:48:28 -0500

    primera subida

---
+++ Curso/Curso1/primero.js
@@ -0,0 +1,8 @@
+const frutas=['mora', 'pera', 'manzana','mora','arandanos'];
+frutas.forEach(item=>{
+    console.log(item)
+})
+console.log("\n"+"separación"+"\n")
+frutas.forEach(item=>{
+    console.count(item)
+});
